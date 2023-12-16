d5c4e10d6c63733bdbf57456ac48f26f4ecd117b
sylv0r <sylvian.vidal95@gmail.com>
2023-12-16 14:43:35 +0100

    add init.db example

---
+++ back/db/initDb.js
@@ -0,0 +1,9 @@
+module.exports = {
+  user: "my_user",
+  password: "my_password",
+  host: "db",
+  port: 3306,
+  database: "my_database",
+  charset   : 'utf8mb4',
+  collation : 'utf8mb4_unicode_ci'
+}
